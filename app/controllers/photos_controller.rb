d71616a35421fb3308b4571268ccad1276a8001d
landroide13 <lazaro02@hotmail.com>
2017-08-03 12:04:15 +0700

    all c's

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,7 +15,37 @@ class PhotosController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    puts "PARAMS #{params}"
+    Photo.find(params[:id].to_i).destroy
+    
+    redirect_to root_path
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+    render 'new'
+  end
+
   def photo_params
     params.require(:photo).permit(:username, :url)
   end
+
+  def update
+    puts "PARAMS #{params}"
+    photo = Photo.find(params[:id])
+    photo.update(photo_params)
+
+    # puts new_name = params[:photo][:username]
+    # puts new_url = params[:photo][:url] 
+    # puts target_id = params[:id].to_i
+    # update_photo = Photo.find(target_id)
+    # puts update_photo.username = new_name
+    #puts update_photo.url = new_url
+    photo.save
+
+    redirect_to root_path
+  end
+
+
 end
